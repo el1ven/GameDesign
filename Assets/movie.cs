75c7682b5c4074cc3290a21f7e8be22bde86d06a
YouYue123 <YOUY0004@e.ntu.edu.sg>
2014-11-02 19:02:32 +0800

    Snake material

---
+++ Assets/movie.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movie : MonoBehaviour {
+	public MovieTexture mp;
+	public AudioSource music;
+	// Use this for initialization
+	void Start () {
+		renderer.material.mainTexture=mp;
+		mp.loop=false;
+		mp.Play();
+		music.Play();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time>51){
+			Application.LoadLevel("Menu");
+		}
+	}
+}
